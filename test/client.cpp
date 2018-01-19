1d1b60cd95935c079882d95480be0a749be0bc4e
Guillaume NEDELEC <guillaume.nedelec3308@gmail.com>
2018-01-19 13:38:44 +0100

    code code sockets

--- test/client.cpp
+++ test/client.cpp
@@ -1,35 +1,71 @@
 #include <iostream>
+#include <regex>
 #include "../src/TCPClient.h"
 
 int main(int argc, char *argv[])
 {
-
     TCPClient tcp;
     tcp.setup("10.0.0.1",11999);
-    string msg = "H";
+    string msg = "G";
+    regex test { "^Cur_position: [0-9]{1,3}\.[0-9]+,[0-9]{1,3}\.[0-9]+,[0-9]{1,3}\.[0-9]+$" };
+    regex test2 { "^New_position: [0-9]{1,3}\.[0-9]+,[0-9]{1,3}\.[0-9]+,[0-9]{1,3}\.[0-9]+$" };
     tcp.Send(msg);
     string rec = tcp.receive();
-    if( rec != "0000000000000000000000000000000000")
+    if( regex_match(rec, test))
     {
-        cout << "send H : Ok receive : " << rec << endl;
+        cout << "send G : Ok receive : " << rec << endl;
     }
-    sleep(1);
-    msg = "G";
+    else
+        cout << "ERR G: " << rec << endl;
+
+    usleep(1000);
+
+
+    msg = "H";
     tcp.Send(msg);
     rec = tcp.receive();
-    if( rec != "0000000000000000000000000000000000")
+    if( regex_match(rec, test))
     {
         cout << "send H : Ok receive : " << rec << endl;
     }
-    sleep(1);
-    msg = "G";
+    else
+        cout << "ERR H : " << rec << endl;
+    usleep(1000);
+
+
+    msg = "MR15";
     tcp.Send(msg);
     rec = tcp.receive();
-    if( rec != "0000000000000000000000000000000000")
+    if( regex_match(rec, test))
     {
-        cout << "send H : Ok receive : " << rec << endl;
+        cout << "send MR15 : Ok receive : " << rec << endl;
     }
-    sleep(1);
+    else
+        cout << "ERR MR15 : " << rec << endl;
+    usleep(1000);
+
+
+    msg = "MY30";
+    tcp.Send(msg);
+    rec = tcp.receive();
+    if( regex_match(rec, test))
+    {
+        cout << "send MY30 : Ok receive : " << rec << endl;
+    }
+    else
+        cout << "ERR MY30: " << rec << endl;
+    usleep(1000);
+
+    msg = "MR25;MY15";
+    tcp.Send(msg);
+    rec = tcp.receive();
+    if( regex_match(rec, test))
+    {
+        cout << "send MR25;MY15 : Ok receive : " << rec << endl;
+    }
+    else
+        cout << "ERR MR25;MY15 : " << rec << endl;
+    usleep(1000);
     return 0;
 }
 
